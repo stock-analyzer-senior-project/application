b07e26ec1d896b872b9d64f7d13ff02802e6f7d1
Lucas Liseth <lucas+github@lucasliseth.com>
2023-03-26 17:43:03 -0400

    Add picker to initial screen

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
 
 export default function App() {
+  const [selectedCompany, setSelectedCompany] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch("http://reactnative.dev/movies.json");
+      const json = await response.json();
+    } catch (err) {
+    } finally {
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <Picker
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
+        selectedValue={selectedCompany}
+        onValueChange={(itemValue) => setSelectedCompany(itemValue)}
       >
         <Picker.Item label="Java" value="java" />
         <Picker.Item label="JavaScript" value="js" />
