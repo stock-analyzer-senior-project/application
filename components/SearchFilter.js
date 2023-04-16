e52db5ecc5757fa74633c11c973de3cdd10bb407
tic-Nimmons <n.timmons@ufl.edu>
2023-04-15 21:06:59 -0400

    "Updated formatting on login page, and created sea rch function. Need to add in link ability for each search result"

---
+++ components/SearchFilter.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View, FlatList } from "react-native"
+import React from "react"
+
+const Filter = ({data, input, setInput}) => {
+    for(let i = 0; i < data.length; i++){
+        
+    }
+    return(
+        //Need to make each option a button
+        
+        <View style={{marginTop:10}}>
+            <FlatList data={data} renderItem={({item}) => {
+                if(input === ""){
+                    //meaning there is nothing being shown, so we need to show something
+                    return(
+                        <View style={{marginVertical:10}}>
+                            <Text style={{fontSize:14,fontWeight:"bold"}}>{item.Name}</Text>
+                            <Text style={{borderColor:"gray",borderWidth:1,height:1,marginTop:5}}/>
+                        </View>
+                    )
+                }
+                if(item.Name.toLowerCase().includes(input.toLowerCase())){
+                    return(
+                        <View style={{marginVertical:10}}>
+                            <Text style={{fontSize:14,fontWeight:"bold"}}>{item.Name}</Text>
+                            <Text style={{borderColor:"gray",borderWidth:1,height:1,marginTop:5}}/>
+                        </View>
+                    )
+                }
+            }}/>
+        </View>
+    )
+}
+
+export default Filter;
+const styles = StyleSheet.create({})
