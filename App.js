0c3843c1d1a678b13703c97910e9200928151dd4
Lucas Liseth <lucas+github@lucasliseth.com>
2023-04-16 17:28:08 -0400

    Add formatting file and ESG explanation

--- App.js
+++ App.js
@@ -1,107 +1,96 @@
-import * as React from "react";
-import {Feather} from "react-native-feather";
-import { Button } from "react-native";
+import * as React from 'react';
+import { Feather } from 'react-native-feather';
+import { Button } from 'react-native';
 //import { SearchBar } from 'react-native-elements';
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 //import { Picker } from "@react-native-picker/picker";
-import { useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { TextInput, Pressable, TouchableOpacity, FlatList } from "react-native";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TextInput, Pressable, TouchableOpacity, FlatList } from 'react-native';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 //import filter from "lodash.filter";
-import { TextInputProps } from "react-native";
-import SearchFilter from "./components/SearchFilter"
-import {auth, database} from "./firebase"
+import { TextInputProps } from 'react-native';
+import SearchFilter from './components/SearchFilter';
+import { auth, database } from './firebase';
 //import {database as db} from "./firebase"
-import { Component } from "@firebase/component";
-import { set } from "lodash";
-import { data } from "./db";
-import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Component } from '@firebase/component';
+import { set } from 'lodash';
+import { data } from './db';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
-let email = ''
+let email = '';
 
-function TabNavigator()
-{
-  return(
-  <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Welcome" }}
-        />
-        <Tab.Screen name="Purpose" component={PurposeScreen} />
-        <Tab.Screen name="Explanation" component={ExplanationScreen} />
-        <Tab.Screen name="How" component={HowScreen} />
-        <Tab.Screen name="Search" component={StockInfoStack} />
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }} />
+      <Tab.Screen name="Purpose" component={PurposeScreen} />
+      <Tab.Screen name="Explanation" component={ExplanationScreen} />
+      <Tab.Screen name="How" component={HowScreen} />
+      <Tab.Screen name="Search" component={StockInfoStack} />
     </Tab.Navigator>
-      )
+  );
 }
-
 
 const Stack = createNativeStackNavigator();
 
 const StockInfoStack = () => {
-   
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Search Screen" component={SearchScreen} options={{headerShown : false}}/>
+      <Stack.Screen
+        name="Search Screen"
+        component={SearchScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="Stock Information" component={InfoScreen} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={TabNavigator} options = {{headerShown : false}}/>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
 
 //***Each Page of the Application***
 
 const HomeScreen = ({ navigation, route }) => {
-  return (
-    <View>
-      
-    </View>
-  );
+  return <View></View>;
 };
 const PurposeScreen = ({ navigation, route }) => {
   return (
     <Text>
-      The purpose of this application is to inform users about what ESG data is
-      and how they can use it in informing their investment decisions.
+      The purpose of this application is to inform users about what ESG data is and how they can use
+      it in informing their investment decisions.
     </Text>
   );
 };
 const ExplanationScreen = ({ navigation, route }) => {
   return (
     <Text style={styles.text}>
-      ESG stands for Environmental, Social, and Governance, and ESG data refers
-      to information that is relevant to these three factors. Specifically, ESG
-      data includes information related to a company's environmental impact,
-      social responsibility, and corporate governance practices. Environmental
-      data may include a company's carbon emissions, energy use, waste
-      management practices, and water usage, among other factors. Social data
-      may include information about a company's labor practices, human rights
-      policies, community engagement, and diversity and inclusion efforts.
-      Governance data may include details about a company's management
-      structure, board composition, executive compensation, and ethical
-      standards. ESG data is increasingly being used by investors and other
-      stakeholders to evaluate a company's overall sustainability and potential
-      long-term performance. Companies that prioritize ESG factors are seen as
-      more responsible and better positioned for long-term success in a rapidly
-      changing business landscape.
+      ESG stands for Environmental, Social, and Governance, and ESG data refers to information that
+      is relevant to these three factors. Specifically, ESG data includes information related to a
+      company's environmental impact, social responsibility, and corporate governance practices.
+      Environmental data may include a company's carbon emissions, energy use, waste management
+      practices, and water usage, among other factors. Social data may include information about a
+      company's labor practices, human rights policies, community engagement, and diversity and
+      inclusion efforts. Governance data may include details about a company's management structure,
+      board composition, executive compensation, and ethical standards. ESG data is increasingly
+      being used by investors and other stakeholders to evaluate a company's overall sustainability
+      and potential long-term performance. Companies that prioritize ESG factors are seen as more
+      responsible and better positioned for long-term success in a rapidly changing business
+      landscape.
     </Text>
   );
 };
@@ -110,133 +99,158 @@ const HowScreen = ({ navigation, route }) => {
 };
 
 const SearchScreen = ({ navigation, route }) => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   return (
-    <View style={{ margin: 15, width: "90%",}}>
-      <View style={{padding:10,flexDirection: "row",width: "102%",backgroundColor: "#d9dbda",borderRadius:10,alignItems: "center",}}>
+    <View style={{ margin: 15, width: '90%' }}>
+      <View
+        style={{
+          padding: 10,
+          flexDirection: 'row',
+          width: '102%',
+          backgroundColor: '#d9dbda',
+          borderRadius: 10,
+          alignItems: 'center',
+        }}
+      >
         {/* Feather is the search bar */}
-        <Feather name="search" size={20} color="black" style={{marginLeft: 1,marginRight: 4}}/>
-        <TextInput value={input} onChangeText={(text) => setInput(text)} style = {{fontsize: 15}} placeholder="Search Ticker Here"/>
+        <Feather name="search" size={20} color="black" style={{ marginLeft: 1, marginRight: 4 }} />
+        <TextInput
+          value={input}
+          onChangeText={(text) => setInput(text)}
+          style={{ fontsize: 15 }}
+          placeholder="Search Ticker Here"
+        />
       </View>
-      <SearchFilter data={data} input={input} setInput={setInput}/>
+      <SearchFilter data={data} input={input} setInput={setInput} />
     </View>
   );
-  
 };
 //This is where clicking on a ticker will take you
-const InfoScreen = ({navigation, route}) => {
+const InfoScreen = ({ navigation, route }) => {
   //Before returning here, we need to update info based on what stock it is
   console.log(route.params.paramKey);
   // return <Text>{route.params.paramKey.Symbol}</Text>;
+  let explanation = '';
+  if (route.params.paramKey.Rating === 'AAA' || route.params.paramKey.Rating === 'AA') {
+    explanation = 'Leader';
+  } else if (
+    route.params.paramKey.Rating === 'A' ||
+    route.params.paramKey.Rating === 'BBB' ||
+    route.params.paramKey.Rating === 'BB'
+  ) {
+    explanation = 'Average';
+  } else {
+    explanation = 'Laggard';
+  }
   return (
     <View>
       <Text>Company: {route.params.paramKey.Name}</Text>
       <Text>Company: {route.params.paramKey.Symbol}</Text>
-      <Text>ESG Rating: {route.params.paramKey.Rating}</Text>
+      <Text>
+        ESG Rating: {route.params.paramKey.Rating} ({explanation})
+      </Text>
     </View>
   );
 };
 
-const LoginScreen = ({navigation, route}) => {
-
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
+const LoginScreen = ({ navigation, route }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const auth = getAuth();
 
-  const handleRegister  = () => {
+  const handleRegister = () => {
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
   };
 
   const handleLogin = () => {
-  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log("Signed in as: ", user.email);
-    navigation.navigate("Home", {screen : "How", params : {name : user.email}});
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-
-  }
-  return(
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log('Signed in as: ', user.email);
+        navigation.navigate('Home', { screen: 'How', params: { name: user.email } });
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+  return (
     <View>
-    <Text style={styles.title}> Welcome Back! </Text>
-    <Pressable>
-      <View style={styles.form}>
-        <Text style={styles.label}>Email: </Text>
+      <Text style={styles.title}> Welcome Back! </Text>
+      <Pressable>
+        <View style={styles.form}>
+          <Text style={styles.label}>Email: </Text>
           <TextInput
             autoCapitalize="none"
             autoCompleteType="email"
             autoCorrect={false}
-            value = {email}
-            onChangeText = {text => setEmail(text)}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
             keyboardType="email-address"
             returnKeyType="next"
             style={styles.textInput}
             textContentType="username"
           />
-      </View>
-    </Pressable>
-  <Pressable>
-    <View style={styles.form}>
-      <Text style={styles.label}>Password: </Text>
-      <TextInput
-       autoCapitalize="none"
-       autoCompleteType="password"
-       autoCorrect={false}
-       value = {password}
-       onChangeText = {text => setPassword(text)}
-       returnKeyType="done"
-       secureTextEntry
-       style={styles.textInput}
-       textContentType="password"
-      />
+        </View>
+      </Pressable>
+      <Pressable>
+        <View style={styles.form}>
+          <Text style={styles.label}>Password: </Text>
+          <TextInput
+            autoCapitalize="none"
+            autoCompleteType="password"
+            autoCorrect={false}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            returnKeyType="done"
+            secureTextEntry
+            style={styles.textInput}
+            textContentType="password"
+          />
+        </View>
+      </Pressable>
+      <TouchableOpacity style={styles.submitButton} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
     </View>
-  </Pressable>
-  <TouchableOpacity style={styles.submitButton} onPress={handleLogin}>
-    <Text style={styles.buttonText}>Login</Text>
-  </TouchableOpacity>
-  <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
-    <Text style={styles.buttonText}>Register</Text>
-  </TouchableOpacity>
-  </View>
-  )
-}
+  );
+};
 
 //***STYLESHEET***//
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
-    textAlign : "center",
+    textAlign: 'center',
     margin: 20,
     flex: 1,
     fontSize: 36,
-    left: "50%",
+    left: '50%',
   },
   text: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   textInput: {
     color: '#FFFFFF',
@@ -248,13 +262,13 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     flexDirection: 'row',
     height: 48,
-    borderWidth : 2,
-    width : 300,
-    left : "62%",
+    borderWidth: 2,
+    width: 300,
+    left: '62%',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal : 16,
-    marginBottom : 10,
+    paddingHorizontal: 16,
+    marginBottom: 10,
   },
   label: {
     color: 'rgba(235, 235, 245, 0.6)',
@@ -263,31 +277,31 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     width: 80,
   },
-  submitButton : {
-    height : 50,
-    width : 150,
-    left : "30%",
-    top: "10%",
+  submitButton: {
+    height: 50,
+    width: 150,
+    left: '30%',
+    top: '10%',
     backgroundColor: '#646464',
-    borderRadius: 25
+    borderRadius: 25,
   },
-  registerButton : {
-    height : 50,
-    width : 150,
-    left : "30%",
-    top: "12%",
+  registerButton: {
+    height: 50,
+    width: 150,
+    left: '30%',
+    top: '12%',
     backgroundColor: '#646464',
-    borderRadius: 25
+    borderRadius: 25,
   },
-  buttonText : {
+  buttonText: {
     color: 'rgba(235, 235, 245, 0.6)',
     fontSize: 20,
     fontWeight: '400',
     lineHeight: 20,
     width: 80,
-    left: "22%",
-    top: "30%",
-    textAlign : "center",
-    textAlignVertical : "center"
-  }
+    left: '22%',
+    top: '30%',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
 });
