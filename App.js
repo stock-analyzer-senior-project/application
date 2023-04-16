151e9042e038e7dda2a5cdde9c3a5c6cff827451
Lucas Liseth <lucas+github@lucasliseth.com>
2023-04-16 01:43:30 -0400

    Formatting and printing company and esg data

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Feather} from "react-native-feather";
+import { Feather } from "react-native-feather";
 import { Button } from "react-native";
 //import { SearchBar } from 'react-native-elements';
 import { StatusBar } from "expo-status-bar";
@@ -9,16 +9,19 @@ import { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TextInput, Pressable, TouchableOpacity, FlatList } from "react-native";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 //import filter from "lodash.filter";
 import { TextInputProps } from "react-native";
-import SearchFilter from "./components/SearchFilter"
-import {auth} from "./firebase"
+import SearchFilter from "./components/SearchFilter";
+import { auth } from "./firebase";
 //import {database as db} from "./firebase"
 import { Component } from "@firebase/component";
 import { set } from "lodash";
 import { data } from "./db";
-
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -26,7 +29,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} /> 
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -36,7 +39,7 @@ export default function App() {
         <Stack.Screen name="Explanation" component={ExplanationScreen} />
         <Stack.Screen name="How" component={HowScreen} />
         <Stack.Screen name="Search" component={SearchScreen} />
-        <Stack.Screen name="Stock Information" component={InfoScreen}/>
+        <Stack.Screen name="Stock Information" component={InfoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -102,105 +105,126 @@ const HowScreen = ({ navigation, route }) => {
 const SearchScreen = ({ navigation, route }) => {
   const [input, setInput] = useState("");
   return (
-    <View style={{ margin: 15, width: "90%",}}>
-      <View style={{padding:10,flexDirection: "row",width: "102%",backgroundColor: "#d9dbda",borderRadius:10,alignItems: "center",}}>
+    <View style={{ margin: 15, width: "90%" }}>
+      <View
+        style={{
+          padding: 10,
+          flexDirection: "row",
+          width: "102%",
+          backgroundColor: "#d9dbda",
+          borderRadius: 10,
+          alignItems: "center",
+        }}
+      >
         {/* Feather is the search bar */}
-        <Feather name="search" size={20} color="black" style={{marginLeft: 1,marginRight: 4}}/>
-        <TextInput value={input} onChangeText={(text) => setInput(text)} style = {{fontsize: 15}} placeholder="Search Ticker Here"/>
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1, marginRight: 4 }}
+        />
+        <TextInput
+          value={input}
+          onChangeText={(text) => setInput(text)}
+          style={{ fontsize: 15 }}
+          placeholder="Search Ticker Here"
+        />
       </View>
-      <SearchFilter data={data} input={input} setInput={setInput}/>
+      <SearchFilter data={data} input={input} setInput={setInput} />
     </View>
   );
-  
 };
 //This is where clicking on a ticker will take you
-const InfoScreen = ({navigation, route}) => {
+const InfoScreen = ({ navigation, route }) => {
   //Before returning here, we need to update info based on what stock it is
-  console.log(route.params.paramKey)
+  console.log(route.params.paramKey);
+  // return <Text>{route.params.paramKey.Symbol}</Text>;
   return (
-    <Text>{route.params.paramKey}</Text>
+    <View>
+      <Text>Company: {route.params.paramKey.Name}</Text>
+      <Text>Company: {route.params.paramKey.Symbol}</Text>
+      <Text>ESG Rating: {route.params.paramKey.Rating}</Text>
+    </View>
   );
 };
 
-const LoginScreen = ({navigation, route}) => {
-
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
+const LoginScreen = ({ navigation, route }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const auth = getAuth();
 
-  const handleRegister  = () => {
+  const handleRegister = () => {
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
   };
 
   const handleLogin = () => {
-  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log("Signed in as: ", user.email);
-    navigation.navigate("Home");
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("Signed in as: ", user.email);
+        navigation.navigate("Home");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
 
-  }
-
-  return(
+  return (
     <View>
-    <Text style={styles.title}> Welcome Back! </Text>
-    <Pressable>
-      <View style={styles.form}>
-        <Text style={styles.label}>Email: </Text>
+      <Text style={styles.title}> Welcome Back! </Text>
+      <Pressable>
+        <View style={styles.form}>
+          <Text style={styles.label}>Email: </Text>
           <TextInput
             autoCapitalize="none"
             autoCompleteType="email"
             autoCorrect={false}
-            value = {email}
-            onChangeText = {text => setEmail(text)}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
             keyboardType="email-address"
             returnKeyType="next"
             style={styles.textInput}
             textContentType="username"
           />
-      </View>
-    </Pressable>
-  <Pressable>
-    <View style={styles.form}>
-      <Text style={styles.label}>Password: </Text>
-      <TextInput
-       autoCapitalize="none"
-       autoCompleteType="password"
-       autoCorrect={false}
-       value = {password}
-       onChangeText = {text => setPassword(text)}
-       returnKeyType="done"
-       secureTextEntry
-       style={styles.textInput}
-       textContentType="password"
-      />
+        </View>
+      </Pressable>
+      <Pressable>
+        <View style={styles.form}>
+          <Text style={styles.label}>Password: </Text>
+          <TextInput
+            autoCapitalize="none"
+            autoCompleteType="password"
+            autoCorrect={false}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            returnKeyType="done"
+            secureTextEntry
+            style={styles.textInput}
+            textContentType="password"
+          />
+        </View>
+      </Pressable>
+      <TouchableOpacity style={styles.submitButton} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
     </View>
-  </Pressable>
-  <TouchableOpacity style={styles.submitButton} onPress={handleLogin}>
-    <Text style={styles.buttonText}>Login</Text>
-  </TouchableOpacity>
-  <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
-    <Text style={styles.buttonText}>Register</Text>
-  </TouchableOpacity>
-  </View>
-  )
-}
+  );
+};
 
 //***STYLESHEET***//
 
@@ -212,7 +236,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   title: {
-    textAlign : "center",
+    textAlign: "center",
     margin: 20,
     flex: 1,
     fontSize: 36,
@@ -225,55 +249,55 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   textInput: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     flex: 1,
   },
   form: {
-    alignItems: 'center',
-    backgroundColor: 'rgb(58, 58, 60)',
+    alignItems: "center",
+    backgroundColor: "rgb(58, 58, 60)",
     borderRadius: 8,
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 48,
-    borderWidth : 2,
-    width : 300,
-    left : "62%",
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal : 16,
-    marginBottom : 10,
+    borderWidth: 2,
+    width: 300,
+    left: "62%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginBottom: 10,
   },
   label: {
-    color: 'rgba(235, 235, 245, 0.6)',
+    color: "rgba(235, 235, 245, 0.6)",
     fontSize: 15,
-    fontWeight: '400',
+    fontWeight: "400",
     lineHeight: 20,
     width: 80,
   },
-  submitButton : {
-    height : 50,
-    width : 150,
-    left : "30%",
+  submitButton: {
+    height: 50,
+    width: 150,
+    left: "30%",
     top: "10%",
-    backgroundColor: '#646464',
-    borderRadius: 25
+    backgroundColor: "#646464",
+    borderRadius: 25,
   },
-  registerButton : {
-    height : 50,
-    width : 150,
-    left : "30%",
+  registerButton: {
+    height: 50,
+    width: 150,
+    left: "30%",
     top: "12%",
-    backgroundColor: '#646464',
-    borderRadius: 25
+    backgroundColor: "#646464",
+    borderRadius: 25,
   },
-  buttonText : {
-    color: 'rgba(235, 235, 245, 0.6)',
+  buttonText: {
+    color: "rgba(235, 235, 245, 0.6)",
     fontSize: 20,
-    fontWeight: '400',
+    fontWeight: "400",
     lineHeight: 20,
     width: 80,
     left: "22%",
     top: "30%",
-    textAlign : "center",
-    textAlignVertical : "center"
-  }
+    textAlign: "center",
+    textAlignVertical: "center",
+  },
 });
